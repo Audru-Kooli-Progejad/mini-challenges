430e89385e5fd9ae1a8bef25baacaab952f5421a
Kristo Koert <kristo.koert@gmail.com>
2015-05-06 08:48:03 +0300

    Refactoring

--- tests.py
+++ tests.py
@@ -1,48 +1,48 @@
 import unittest
-import conditionals
+import conditionals as cond
 
 
 class ConditionalsTests(unittest.TestCase):
 
     def test_is_ten(self):
-        self.assertEqual(True, conditionals.is_ten(10))
-        self.assertEqual(False, conditionals.is_ten(4))
+        self.assertEqual(True, cond.is_ten(10))
+        self.assertEqual(False, cond.is_ten(4))
 
     def test_bigger_than_ten(self):
-        self.assertEqual(False, conditionals.bigger_than_ten(10))
-        self.assertEqual(False, conditionals.bigger_than_ten(0))
-        self.assertEqual(True, conditionals.bigger_than_ten(100))
+        self.assertEqual(False, cond.bigger_than_ten(10))
+        self.assertEqual(False, cond.bigger_than_ten(0))
+        self.assertEqual(True, cond.bigger_than_ten(100))
 
     def test_smaller_than_ten(self):
-        self.assertEqual(False, conditionals.smaller_than_ten(10))
-        self.assertEqual(True, conditionals.smaller_than_ten(0))
-        self.assertEqual(False, conditionals.smaller_than_ten(100))
+        self.assertEqual(False, cond.smaller_than_ten(10))
+        self.assertEqual(True, cond.smaller_than_ten(0))
+        self.assertEqual(False, cond.smaller_than_ten(100))
 
     def test_return_bigger(self):
-        self.assertEqual(10, conditionals.return_bigger(0, 10))
-        self.assertEqual(0, conditionals.return_bigger(0, 0))
+        self.assertEqual(10, cond.return_bigger(0, 10))
+        self.assertEqual(0, cond.return_bigger(0, 0))
 
     def test_is_positive(self):
-        self.assertEqual(True, conditionals.is_positive(10))
-        self.assertEqual(False, conditionals.is_positive(-10))
+        self.assertEqual(True, cond.is_positive(10))
+        self.assertEqual(False, cond.is_positive(-10))
 
     def test_is_negative(self):
-        self.assertEqual(False, conditionals.is_negative(10))
-        self.assertEqual(True, conditionals.is_negative(-10))
+        self.assertEqual(False, cond.is_negative(10))
+        self.assertEqual(True, cond.is_negative(-10))
 
-    def test_is_divisable_by_two(self):
+    def test_is_divisible_by_two(self):
         pass
 
-    def test_is_divisable_by_five(self):
+    def test_is_divisible_by_five(self):
         pass
 
-    def test_is_divisable_by_ten(self):
+    def test_is_divisible_by_ten(self):
         pass
 
-    def test_is_divisable_by_ten_and_five(self):
+    def test_is_divisible_by_ten_and_five(self):
         pass
 
-    def test_is_divisable_by_ten_or_five(self):
+    def test_is_divisible_by_ten_or_five(self):
         pass
 
     def test_is_str_longer_than_ten(self):
